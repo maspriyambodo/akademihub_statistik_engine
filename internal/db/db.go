d772033a4b8ed03c0701e9564308401bdb8013f1
maspriyambodo <maspriyambodo>
2026-04-18 15:15:36 +0700

    resolve error connection pgbouncer

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,16 +3,23 @@ package db
 import (
 	"time"
 
+	pgxv5 "github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
 	"github.com/sekolahpintar/statistik-engine/internal/config"
 )
 
 func New(cfg *config.Config) (*sqlx.DB, error) {
-	database, err := sqlx.Open("postgres", cfg.DB.DSN())
+	connConfig, err := pgxv5.ParseConfig(cfg.DB.DSN())
 	if err != nil {
 		return nil, err
 	}
+	// Use simple protocol so pgbouncer transaction-pooling mode does not
+	// break on cached prepared statements ("unnamed prepared statement does
+	// not exist").
+	connConfig.DefaultQueryExecMode = pgxv5.QueryExecModeSimpleProtocol
+	stdDB := stdlib.OpenDB(*connConfig)
+	database := sqlx.NewDb(stdDB, "pgx")
 
 	database.SetMaxOpenConns(30)
 	database.SetMaxIdleConns(10)
